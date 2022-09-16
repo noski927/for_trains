1290df38083bf72513ba2a79930fe5683108d7b3
George <go.dmit@mail.ru>
2022-09-16 18:08:38 +0300

    add aaa

--- Leetcode/566_Reshape_the_Matrix.py
+++
@@ -1,8 +0,0 @@
-def matrixReshape(mat, r, c):
-    if (len(mat) * len(mat[0])) <= (r * c) | len(mat) % c == 0 | len(mat[0]) % r == 0:
-        print(True)
-    else:
-        print(False)
-
-
-matrixReshape([[1, 2], [3, 4], [5,6]],1,4)  
